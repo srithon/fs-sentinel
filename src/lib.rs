169b65477996a70e4f073df73c7b8b55aa56e7e1
Sridaran Thoniyil <sri7thon@gmail.com>
2023-08-21 14:58:19 -0400

    Change status bool to enum

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,16 @@ mod linux;
 
 pub type FileSystemID = String;
 
+pub enum FileSystemStatus {
+    Modified,
+    UnModified
+}
+
 /// State maps filesystems to whether or not they were modified.
 pub struct Daemon<P: Platform> {
     platform: P,
-    filesystem_statuses: RwLock<HashMap<FileSystemID, Mutex<bool>>>,
     filesystem_futures: FuturesUnordered<FileSystemID>,
+    filesystem_statuses: RwLock<HashMap<FileSystemID, Mutex<FileSystemStatus>>>,
 /// A FileSystem combines a FileSystemID with a path contained within the FileSystem; for any
 /// requirements concerning the path itself, see the documentation for your used `Platform`.
 pub struct FileSystem {
